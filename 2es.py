6c1039368e6ad62ce77677cb7f8959d68bfd84cb
AffuBlog <104565027+AffuBlog@users.noreply.github.com>
2022-12-22 09:13:33 +0100

    Add files via upload

---
+++ 2es.py
@@ -0,0 +1,17 @@
+def get_hours_minutes_seconds(seconds):
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+     
+    return str(hour)+"h "+str(minutes)+"m "+str(seconds)+"s"
+    
+
+print(get_hours_minutes_seconds(30))
+print(get_hours_minutes_seconds(60))
+print(get_hours_minutes_seconds(90))
+print(get_hours_minutes_seconds(3600))
+print(get_hours_minutes_seconds(3601))
+print(get_hours_minutes_seconds(3661))
+print(get_hours_minutes_seconds(90042))
+print(get_hours_minutes_seconds(0))
